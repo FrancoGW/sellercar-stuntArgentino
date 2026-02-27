2ad00ffb8ce65682f7bcdb844e3ff2aa429ccf2f
Franco Lautaro Garay <lautarofrancogaray.unne@hotmail.com>
2026-02-27 14:36:55 -0300

    .

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Entrada para Vercel. Se empaqueta con `ncc` en el build para incluir todo el backend en un solo archivo.
- * No cambiar el require: ncc lo usa para hacer el bundle desde backend/dist.
- */
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { AppFactory } = require('../backend/dist/app.factory');
-
-module.exports = AppFactory.create().expressApp;
